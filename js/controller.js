b346410a459f6ea0ab1a47d2d79ea6f3c4a7b0b6
Julien Duribreux <julien.duribreux@inria.fr>
2015-07-10 16:37:11 +0200

    fix: remove unused information in infoWindow

--- js/controller.js
+++ js/controller.js
@@ -42,7 +42,7 @@ function createRouterMarker(coordinates) {
 }
 
 function getRandomColor() {
-    var letters = '0123456789AB'.split('');
+    var letters = '0123456789ABCD'.split('');
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
@@ -55,8 +55,8 @@ function drawRequestsPath(coordinates, url) {
 		path: coordinates,
 		geodesic: true,
 		strokeColor: getRandomColor(),
-		strokeOpacity: 0.8,
-		strokeWeight: 1
+		strokeOpacity: 0.5,
+		strokeWeight: 2
 	});
 
 	flightPath.setMap(map);
@@ -77,18 +77,13 @@ function initializeNetworkMap() {
 	                var scan_ping = info.scan_ping;
 	                var scan_ttl = info.scan_ttl;
 	                var scan_trace = info.scan_trace;
-	                
-	                var download_url = info.download_url;
-	                var download_speed_mbps = info.download_speed_mbps;
-	                var download_time_seconds = info.download_time_seconds;
-	                var download_size_bytes = info.download_size_bytes;
 
 					setGoogleMapPosition(info.latitude,info.longitude);
 
 					var myLatlng = new google.maps.LatLng(info.latitude,info.longitude);
 
-					var meta_os = "unknwon";
-					var meta_time = "unknwon";
+					var meta_os = "N/A";
+					var meta_time = "N/A";
 
 					// Metadata
 					if (data[i].hasOwnProperty('metadata')) {
@@ -125,8 +120,7 @@ function initializeNetworkMap() {
 					var contentString =
 						"<h4>Metadata</h4><p>Device OS : " + meta_os + "<br/>At : " + meta_time + "</p>" +
 						"<h4>Geolocalisation</h4><p>Latitude : " + latitude + "<br/>Longitude : " + longitude + "</p>" +
-						"<h4>Scan</h4><p>Target : " + scan_url + "<br/>Latency : " + scan_ping + " ms<br/>TTL max : " + scan_ttl + "<br/>Traceroute :<br/>" + trace_output +"</p>" +
-						"<h4>Download Stats</h4><p>Target : " + download_url + "<br/>Speed : " + download_speed_mbps + " mbps<br/>File size : " + download_size_bytes + " bytes<br/>Time : " + download_time_seconds + " seconds</p>";
+						"<h4>Scan</h4><p>Target : " + scan_url + "<br/>Latency : " + scan_ping + " ms<br/>TTL max : " + scan_ttl + "<br/>Traceroute :<br/>" + trace_output +"</p>";
 
 					var infowindow = new google.maps.InfoWindow({
 					  content: contentString
